d1fc007aabb01fcb8a38f7765a9f15a3cd246da9
George Lewis <ghl227@gmail.com>
2023-03-26 15:23:27 -0400

    Add simulation script

---
+++ simulate.py
@@ -0,0 +1,43 @@
+import dm_control.mujoco as mujoco
+from dm_control.mujoco.wrapper.mjbindings import enums
+
+
+def simulate(physics, duration=10, frames_per_sec=60):
+    # Get the model timestep:
+    timestep = physics.timestep()
+    last_frame_time = None
+    seconds_per_frame = 1.0 / frames_per_sec
+
+    # Simulate and display video.
+    states = []
+    frames = []
+
+    # Visualize the joint axis
+    scene_option = mujoco.wrapper.core.MjvOption()
+    scene_option.flags[enums.mjtVisFlag.mjVIS_JOINT] = True
+
+    while physics.time() < duration:
+        time = physics.time()
+
+        # Set the values
+        physics.named.data.ctrl["elbow_motor"] = 0.1
+
+        # Get the state of the model at the start of the time window
+        states.append(
+            {
+                "time": physics.time(),
+                "pos": physics.named.data.geom_xpos,
+                "mat": physics.named.data.geom_xmat,
+            }
+        )
+
+        if last_frame_time is None or time > last_frame_time + seconds_per_frame:
+            # renderer.update_scene(data)
+            pixels = physics.render(scene_option=scene_option)
+            frames.append(pixels)
+            last_frame_time = time
+
+        # Jump to the next state
+        physics.step()
+
+    return states, frames
