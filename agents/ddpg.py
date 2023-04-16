8bb5be609810f6f218004eda7bdd1ee740f6f581
George Lewis <ghl227@gmail.com>
2023-04-16 16:22:16 -0400

    Cleaning up ddpg

--- agents/ddpg.py
+++ agents/ddpg.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 import tensorflow as tf
 from tensorflow.keras.layers import BatchNormalization
@@ -16,13 +16,41 @@ from transition_history import TransitionHistory
 from transition import Transition
 
 
+@dataclass
+class Stats:
+    actor_losses: list = field(default_factory=list)
+    critic_losses: list = field(default_factory=list)
+    episode_rewards: list = field(default_factory=list)
+    critic_nmses: list = field(default_factory=list)
+
+    def clear(self):
+        self.actor_losses.clear()
+        self.critic_losses.clear()
+        self.episode_rewards.clear()
+        self.critic_nmses.clear()
+
+    def write_summaries(self, episode_num):
+        tf.summary.scalar(
+            "avg_actor_loss", np.mean(self.actor_losses), step=episode_num
+        )
+        tf.summary.scalar(
+            "avg_critic_loss", np.mean(self.critic_losses), step=episode_num
+        )
+        tf.summary.scalar(
+            "avg_critic_nmse", np.mean(self.critic_nmses), step=episode_num
+        )
+        tf.summary.scalar(
+            "episode_reward", np.sum(self.episode_rewards), step=episode_num
+        )
+
+
 # Actor network or Policy network
 # The actor takes a state and returns an action.
 # It determines what the next action should be.
 def create_actor(state_dim, action_dim, action_bound):
     inputs = layers.Input(shape=(state_dim,))
     x = layers.Dense(
-        40,
+        10,
         kernel_initializer=HeNormal(),
         bias_initializer=Zeros(),
         kernel_regularizer=l2(1e-5),
@@ -30,7 +58,7 @@ def create_actor(state_dim, action_dim, action_bound):
     x = BatchNormalization()(x)
     x = layers.ReLU()(x)
     x = layers.Dense(
-        30,
+        5,
         kernel_initializer=HeNormal(),
         bias_initializer=Zeros(),
         kernel_regularizer=l2(1e-5),
@@ -51,7 +79,7 @@ def create_critic(state_dim, action_dim):
     action_inputs = layers.Input(shape=(action_dim,))
     inputs = layers.Concatenate()([state_inputs, action_inputs])
     x = layers.Dense(
-        40,
+        10,
         kernel_initializer=HeNormal(),
         bias_initializer=Zeros(),
         kernel_regularizer=l2(1e-5),
@@ -59,7 +87,7 @@ def create_critic(state_dim, action_dim):
     x = BatchNormalization()(x)
     x = layers.ReLU()(x)
     x = layers.Dense(
-        30,
+        5,
         kernel_initializer=HeNormal(),
         bias_initializer=Zeros(),
         kernel_regularizer=l2(1e-5),
@@ -96,6 +124,8 @@ class DDPGAgent(BaseAgent):
         self.critic = create_critic(state_dim, action_dim)
         self.critic_target = create_critic(state_dim, action_dim)
 
+        self.stats = Stats()
+
         # Action noise
         self.noise = OrnsteinUhlenbeckActionNoise(
             mean=np.zeros(action_dim),
@@ -126,18 +156,6 @@ class DDPGAgent(BaseAgent):
             learning_rate=critic_lr_schedule
         )
 
-        self.episode_actor_losses = []
-        self.episode_critic_losses = []
-        self.episode_rewards = []
-
-        self._train_batch_func = self._train_batch.get_concrete_function(
-            tf.TensorSpec(shape=(None, self.state_dim), dtype=tf.float32),
-            tf.TensorSpec(shape=(None, self.action_dim), dtype=tf.float32),
-            tf.TensorSpec(shape=(None, self.state_dim), dtype=tf.float32),
-            tf.TensorSpec(shape=(None, 1), dtype=tf.float32),
-            tf.TensorSpec(shape=(None, 1), dtype=tf.float32),
-        )
-
     @tf.function
     def get_action(self, state, noise=True):
         flattened_state = self._flatten_state(state)
@@ -155,15 +173,13 @@ class DDPGAgent(BaseAgent):
             reward=reward,
             done=done,
         )
-        self.episode_rewards.append(reward)
         self.memory.add_transition(transition)
-
         if len(self.memory) < self.params.batch_size:
             return
 
         batch = self.memory.get_batch(self.params.batch_size)
 
-        critic_loss, actor_loss, target_q_values = self._train_batch(
+        training_metrics = self._train_batch(
             states=batch.states,
             actions=batch.actions,
             next_states=batch.next_states,
@@ -171,20 +187,16 @@ class DDPGAgent(BaseAgent):
             dones=batch.dones,
         )
 
-        # Accumulate the losses for summaries
-        self.episode_actor_losses.append(actor_loss.numpy())
-        # Convert the loss to a RMS and scale by the target q values to make it a percentage
-        # This avoids the loss increasing as q increases, which is a bit odd for Tensorboard
-        self.episode_critic_losses.append(
-            (tf.sqrt(critic_loss) / tf.reduce_mean(target_q_values)).numpy()
-        )
+        self.stats.episode_rewards.append(reward)
+        self.stats.actor_losses.append(training_metrics["actor_loss"].numpy())
+        self.stats.critic_losses.append(training_metrics["critic_loss"].numpy())
+        self.stats.critic_nmses.append(training_metrics["critic_nmse"].numpy())
 
     @tf.function
     def _train_batch(self, states, actions, next_states, rewards, dones):
         #
         # Calculate the 'target' q-value using target netowrks
         #
-
         # Given the next state, pick a next action
         target_actions = self.actor_target(next_states)
         # Then, calculate the q-value for that (next_state, next_action)
@@ -205,7 +217,15 @@ class DDPGAgent(BaseAgent):
             q_values = self.critic([states, actions])
             # and we compare it to our target q-value and use that to derive the loss.
             critic_loss = tf.reduce_mean(tf.square(target_q_values - q_values))
+            # Calculate this as a metric
+            critic_nmse = tf.reduce_mean(
+                tf.math.divide_no_nan(
+                    tf.square(target_q_values - q_values),
+                    tf.square(target_q_values),
+                ),
+            )
             critic_grads = tape.gradient(critic_loss, self.critic.trainable_weights)
+
         critic_grads, _ = tf.clip_by_global_norm(critic_grads, clip_norm=1.0)
         self.critic_optimizer.apply_gradients(
             zip(critic_grads, self.critic.trainable_weights)
@@ -222,6 +242,7 @@ class DDPGAgent(BaseAgent):
             # And derive a loss from that q-value
             actor_loss = -tf.reduce_mean(q_values)
             actor_grads = tape.gradient(actor_loss, self.actor.trainable_weights)
+
         actor_grads, _ = tf.clip_by_global_norm(actor_grads, clip_norm=1.0)
         self.actor_optimizer.apply_gradients(
             zip(actor_grads, self.actor.trainable_weights)
@@ -230,7 +251,11 @@ class DDPGAgent(BaseAgent):
         # Update the target networks
         self._update_target_networks()
 
-        return critic_loss, actor_loss, target_q_values
+        return {
+            "critic_loss": critic_loss,
+            "critic_nmse": critic_nmse,
+            "actor_loss": actor_loss,
+        }
 
     def _update_target_networks(self):
         tau = self.params.tau
@@ -241,15 +266,8 @@ class DDPGAgent(BaseAgent):
             target.assign(tau * source + (1 - tau) * target)
 
     def write_summaries(self, episode_num):
-        episode_reward = np.sum(self.episode_rewards)
-        avg_actor_loss = np.mean(self.episode_actor_losses)
-        avg_critic_loss = np.mean(self.episode_critic_losses)
-        tf.summary.scalar("avg_actor_loss", avg_actor_loss, step=episode_num)
-        tf.summary.scalar("avg_critic_loss", avg_critic_loss, step=episode_num)
-        tf.summary.scalar("episode_reward", episode_reward, step=episode_num)
-        self.episode_actor_losses = []
-        self.episode_critic_losses = []
-        self.episode_rewards = []
+        self.stats.write_summaries(episode_num)
+        self.stats.clear()
 
     def _flatten_state(self, state):
         return tf.concat([state[key] for key in sorted(state.keys())], axis=0)
