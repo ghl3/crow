ab4432a05e9a3b498b11971b55b20e23eece89ee
George Lewis <ghl227@gmail.com>
2023-06-03 17:23:18 -0400

    Fix qlearning

--- agents/qlearning.py
+++ agents/qlearning.py
@@ -1,90 +1,95 @@
 # agents/qlearning.py
-
-
-import random
 import tensorflow as tf
 from tensorflow.keras import layers
 import tensorflow.experimental.numpy as np
 
 
-class ReplayBuffer:
-    def __init__(self, max_size):
-        self.buffer = []
-        self.max_size = max_size
-
-    def add(self, state, action, reward, next_state, done):
-        if len(self.buffer) >= self.max_size:
-            self.buffer.pop(0)
-        self.buffer.append((state, action, reward, next_state, done))
-
-    def sample(self, batch_size):
-        return random.sample(self.buffer, batch_size)
-
-
 class QLearningAgent:
     def __init__(
-        self,
-        state_dim,
-        action_dim,
-        learning_rate=0.01,
-        gamma=0.99,
-        batch_size=64,
-        buffer_size=10000,
+        self, state_dim, action_dim, learning_rate=0.01, gamma=0.99, epsilon=0.1
     ):
         self.state_dim = state_dim
         self.action_dim = action_dim
         self.learning_rate = learning_rate
         self.gamma = gamma
-        self.batch_size = batch_size
-        self.buffer = ReplayBuffer(buffer_size)
-        self.optimizer = tf.keras.optimizers.legacy.Adam(
-            learning_rate=self.learning_rate
-        )
-        self.model = self.create_q_model()
+        self.epsilon = epsilon
 
-    def create_q_model(self):
-        model = tf.keras.models.Sequential(
+        # Initialize Q-Network
+        self.model = tf.keras.Sequential(
             [
-                layers.Dense(32, activation="relu", input_shape=(self.state_dim,)),
+                layers.Dense(32, input_shape=(state_dim,), activation="relu"),
                 layers.Dense(32, activation="relu"),
-                layers.Dense(self.action_dim, activation="linear"),
+                layers.Dense(action_dim),
             ]
         )
-        return model
+        self.optimizer = tf.keras.optimizers.legacy.Adam(
+            learning_rate=float(learning_rate)
+        )
 
-    def get_action(self, state, epsilon=0):
-        if np.random.rand() <= epsilon:
-            return np.random.choice(self.action_dim)  # Random action (explore)
-        else:
-            q_values = self.model(state[None, :], training=False)
-            return np.argmax(q_values[0])  # Best action (exploit)
+        # Add an attribute to hold loss stats
+        self.loss_stats = []
 
-    def remember(self, state, action, reward, next_state, done):
-        self.buffer.add(state, action, reward, next_state, done)
+    def _flatten_state(self, state):
+        return tf.concat([state[key] for key in sorted(state.keys())], axis=0)
 
     @tf.function
-    def train(self):
-        if len(self.buffer.buffer) < self.batch_size:
-            return
+    def get_action(self, state, noise=False):
+        state = self._flatten_state(state)
+        state = tf.expand_dims(state, axis=0)  # add batch dimension
+        # epsilon = self.epsilon if epsilon is None else epsilon
+        # if np.random.rand() < epsilon:
+        #    return np.random.choice(self.action_dim)  # Random action (explore)
+        # else:
+        q_values = self.model(state, training=False)
+        return np.argmax(q_values[0])
 
-        batch = self.buffer.sample(self.batch_size)
-        states, actions, rewards, next_states, dones = map(np.array, zip(*batch))
-        states = states.reshape(self.batch_size, self.state_dim)
-        next_states = next_states.reshape(self.batch_size, self.state_dim)
+    def train(self, state, action, next_state, reward, done):
+        state = self._flatten_state(state)
+        next_state = self._flatten_state(next_state)
 
+        loss = self.train_step(state, action, next_state, reward, done)
+
+        # Save the loss stats
+        self.loss_stats.append(loss.numpy())
+
+        return loss
+
+    @tf.function
+    def train_step(self, state, action, next_state, reward, done):
         with tf.GradientTape() as tape:
-            q_values = self.model(states, training=True)
-            q_next = tf.stop_gradient(self.model(next_states, training=False))
-            # Q-learning update
-            target_q_values = q_values.numpy()
-            max_q_next = np.amax(q_next, axis=1)
-            for i, action in enumerate(actions):
-                if dones[i]:
-                    target_q_values[i][action] = rewards[i]
-                else:
-                    target_q_values[i][action] = rewards[i] + self.gamma * max_q_next[i]
-            target_q_values = tf.convert_to_tensor(target_q_values)
-            loss = tf.reduce_mean(tf.square(target_q_values - q_values))
+            state = tf.expand_dims(state, axis=0)  # add batch dimension
+            next_state = tf.expand_dims(next_state, axis=0)  # add batch dimension
 
-        gradients = tape.gradient(loss, self.model.trainable_variables)
-        self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
+            # Calculate Q(s, a)
+            q_values = self.model(state)
+            q_sa = tf.gather(q_values[0], action)
+
+            # Calculate max_a' Q(s', a')
+            next_q_values = self.model(next_state)
+            q_s_a_prime = tf.reduce_max(next_q_values[0])
+
+            # Convert reward, done, and self.gamma to same type
+            reward = tf.cast(reward, tf.float32)
+            done = tf.cast(done, tf.float32)
+            gamma = tf.cast(self.gamma, tf.float32)
+
+            # Calculate target for Q(s, a)
+            q_target_sa = reward + (1.0 - done) * gamma * q_s_a_prime
+
+            # Calculate loss
+            loss = tf.square(q_sa - q_target_sa)
+
+        # Optimize
+        grads = tape.gradient(loss, self.model.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.model.trainable_weights))
+
+        # Save the loss stats
+        # elf.loss_stats.append(loss.numpy())
+
+        return loss
+
+    def write_summaries(self, episode):
+        avg_loss = np.mean(self.loss_stats)
+        tf.summary.scalar("avg_loss", avg_loss, step=episode)
+        # Clear stats
+        self.loss_stats.clear()
